7ffc1d8a5ff09b19f13970dc940d973085104279
Rahul <Rahulsingh4533@gmail.com>
2023-12-26 01:59:55 +0530

    adding clear button to clear the screen or set the values to 0

--- src/itemContext.js
+++ src/itemContext.js
@@ -1,34 +1,38 @@
-import { createContext,useState,useContext } from "react";
+import { createContext, useState, useContext } from "react";
 
-const itemcontext=createContext();
+const itemcontext = createContext();
 
-function useValue(){
-    const value=useContext(itemcontext);
-    return value;
+function useValue() {
+  const value = useContext(itemcontext);
+  return value;
 }
-export {useValue};
+export { useValue };
 
-function CustumIteamProvider({children}){
-   
-    const [total, setTotal] = useState(0);
-    const [item, setItem] = useState(0);
-    const handleAdd = (price) => {
-        setTotal(price + total);
-        setItem((item +1));
-      };
-    
-      const handleRemove = (price) => {
-        if (total <= 0) {
-          return;
-        }
-        setTotal((prevState) => prevState - price);
-        setItem((item -1));
-      };
-    return(
-        <itemcontext.Provider value={{total,setTotal,item,setItem,handleAdd,handleRemove}}>
-          {children}
-        </itemcontext.Provider>
-    )
+function CustumIteamProvider({ children }) {
+  const [total, setTotal] = useState(0);
+  const [item, setItem] = useState(0);
+  const handleAdd = (price) => {
+    setTotal(price + total);
+    setItem(item + 1);
+  };
+
+  const handleRemove = (price) => {
+    if (total <= 0) {
+      return;
+    }
+    setTotal((prevState) => prevState - price);
+    setItem(item - 1);
+  };
+  const clearScreen=()=>{
+      setItem(0);
+      setTotal(0);
+
+  }
+  return (
+    <itemcontext.Provider value={{ total, item, handleAdd, handleRemove ,clearScreen}}>
+      {children}
+    </itemcontext.Provider>
+  );
 }
 
 export default CustumIteamProvider;
